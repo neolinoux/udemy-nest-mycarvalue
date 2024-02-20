9fb12d9e49179290be8ed233d766dfa86a6984b2
neolinoux <rahmadsetiyabudi@gmail.com>
2024-02-21 01:19:18 +0700

    some update in report endpoint

--- src/reports/entities/report.entity.ts
+++ src/reports/entities/report.entity.ts
@@ -7,4 +7,22 @@ export class Report {
 
   @Column()
   price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  long: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  milage: number;
 }
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dto/create-report.dto';
 import { UpdateReportDto } from './dto/update-report.dto';
+import { AuthGuard } from 'src/guards/auth.guards';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
   @Post()
-  create(@Body() createReportDto: CreateReportDto) {
-    return this.reportsService.create(createReportDto);
+  @UseGuards(AuthGuard)
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
   }
 
   @Get()
